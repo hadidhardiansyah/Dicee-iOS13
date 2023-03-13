dbcd78479d80509609b852a94e48f6eefb47765f
Hadid Hardiansyah Saputra <hadidhardiansyah@gmail.com>
2023-03-13 13:21:11 +0700

    Make random dice numbers when click Roll button

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -14,16 +14,28 @@ class ViewController: UIViewController {
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
     
-    var leftDiceNumber = 1
-    var rightDiceNumber = 5
-
     @IBAction func rollButtonPressed(_ sender: UIButton) {
         
-        diceImageView1.image = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")][leftDiceNumber]
-        diceImageView2.image = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")][rightDiceNumber]
+        let diceArray = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
         
-        leftDiceNumber += 1
-        rightDiceNumber -= 1
+        diceImageView1.image = diceArray.randomElement()
+        diceImageView2.image = diceArray.randomElement()
+        
+        // OR
+        /*
+         diceImageView1.image = diceArray[Int.random(in: 1...(diceArray.count - 1))]
+         diceImageView2.image = diceArray[Int.random(in: 1...(diceArray.count - 1))]
+         */
+        
+        /*
+         diceImageView1.image = diceArray[Int.random(in: 1...5)]
+         diceImageView2.image = diceArray[Int.random(in: 1...5)]
+         */
+        
+        /*
+         diceImageView1.image = diceArray[Int.random(in: 1..<6)]
+         diceImageView2.image = diceArray[Int.random(in: 1..<6)]
+         */
         
     }
     
